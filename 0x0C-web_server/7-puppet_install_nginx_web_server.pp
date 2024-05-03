d5cb78b869a215d8aea5838dc81870694e30c37f
Tosinkuzzy <Adeleye608@gmail.com>
2024-05-03 04:37:46 +0100

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,11 +3,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/tosinkuzzy permanent;',
 }
 
 file { '/var/www/html/index.html':
